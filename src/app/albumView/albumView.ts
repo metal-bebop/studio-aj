41f626843171a9b6e7775fce05e49ed094d6dfc7
r2andrew <abradley4646@gmail.com>
2025-07-31 18:54:50 +0100

    lightbox working

--- src/app/albumView/albumView.ts
+++ src/app/albumView/albumView.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import GLightbox from 'glightbox';
 import { NgxMasonryModule } from 'ngx-masonry';
 
 @Component({
@@ -14,10 +15,32 @@ export class AlbumViewComponent {
   images: string[] = [];
   title: string = '';
 
+  private lightbox: any;
+
   constructor(private router: Router) {
     const nav = this.router.getCurrentNavigation();
     const state = nav?.extras.state as { images?: string[]; title?: string };
     this.images = state?.images ?? [];
     this.title = state?.title ?? '';
+  }
+
+  ngAfterViewInit() {
+    this.lightbox = GLightbox({
+      elements: this.images.map((img) => ({
+        href: img,
+      })) as any,
+    });
+  }
+
+  openLightbox(index: number) {
+    if (this.lightbox) {
+      this.lightbox.openAt(index);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.lightbox) {
+      this.lightbox.destroy();
+    }
   }
 }
